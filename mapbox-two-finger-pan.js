9be72356f84c6348ee77e081db44f4f674e52a34
Peter Bailey <hi@peabay.xyz>
2018-05-01 16:42:32 +0100

    Migration from a class to static functions

--- mapbox-two-finger-pan.js
+++ mapbox-two-finger-pan.js
@@ -2,60 +2,61 @@
  * Two Finger Mapbox Panning
  * new twoFingerMapboxPan(<mapbox instance>)
  */
-export class twoFingerMapboxPan {
-  constructor (mapbox) {
-    this.mapbox = mapbox
-    this.panStart = { x: 0, y: 0 }
-    this.init()
-  }
-  init () {
-    this.mapbox.getContainer().addEventListener('touchstart', this.touchStart.bind(this), false)
-    this.mapbox.getContainer().addEventListener('touchmove', this.touchMove.bind(this), false)
-    if ('ontouchstart' in document.documentElement) this.mapbox.dragPan.disable()
-  }
-  touchStart (event) {
-    if (event.touches.length >= 2) {
-      event.stopImmediatePropagation()
-      event.preventDefault()
+const state = {
+  mapbox: null,
+  panStart: { x: 0, y: 0 }
+}
 
-      let x = 0
-      let y = 0
+export default (mapbox) => {
+  state.mapbox = mapbox
+  state.mapbox.getContainer().addEventListener('touchstart', touchStart, false)
+  state.mapbox.getContainer().addEventListener('touchmove', touchMove, false)
+  if ('ontouchstart' in document.documentElement) state.mapbox.dragPan.disable()
+}
 
-      for (let touch of Array.from(event.touches)) {
-        x += touch.screenX
-        y += touch.screenY
-      }
+function touchStart (event) {
+  if (event.touches.length === 2) {
+    event.stopImmediatePropagation()
+    event.preventDefault()
 
-      this.panStart.x = x / event.touches.length
-      this.panStart.y = y / event.touches.length
+    let x = 0
+    let y = 0
+
+    for (let touch of Array.from(event.touches)) {
+      x += touch.screenX
+      y += touch.screenY
     }
+
+    state.panStart.x = x / event.touches.length
+    state.panStart.y = y / event.touches.length
   }
-  touchMove (event) {
-    if (event.touches.length >= 2) {
-      event.stopImmediatePropagation()
-      event.preventDefault()
+}
 
-      let x = 0
-      let y = 0
+function touchMove (event, callback) {
+  if (event.touches.length === 2) {
+    event.stopImmediatePropagation()
+    event.preventDefault()
 
-      for (let touch of Array.from(event.touches)) {
-        x += touch.screenX
-        y += touch.screenY
-      }
+    let x = 0
+    let y = 0
 
-      const movex = (x / event.touches.length) - this.panStart.x
-      const movey = (y / event.touches.length) - this.panStart.y
-
-      this.panStart.x = x / event.touches.length
-      this.panStart.y = y / event.touches.length
-
-      this.mapbox.panBy(
-        [
-          (movex * 1) / -1,
-          (movey * 1) / -1
-        ],
-        { animate: false }
-      )
+    for (let touch of Array.from(event.touches)) {
+      x += touch.screenX
+      y += touch.screenY
     }
+
+    const movex = (x / event.touches.length) - state.panStart.x
+    const movey = (y / event.touches.length) - state.panStart.y
+
+    state.panStart.x = x / event.touches.length
+    state.panStart.y = y / event.touches.length
+
+    state.mapbox.panBy(
+      [
+        (movex * 1) / -1,
+        (movey * 1) / -1
+      ],
+      { animate: false }
+    )
   }
 }
